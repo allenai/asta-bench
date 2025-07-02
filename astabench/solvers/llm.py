32ffb7a40efc8c57d541979c0034449af0e124fa
Mike D'Arcy <miked@allenai.org>
2025-07-02 14:32:02 -0700

    More thorough tests

--- astabench/solvers/llm.py
+++ astabench/solvers/llm.py
@@ -59,7 +59,7 @@ def custom_solve() -> Solver:
                     "tool": "get_paper_batch",
                     "arguments": {
                         "ids": ["arxiv:2409.11363", "arxiv:2409.11364"],
-                        "fields": "title,",
+                        "fields": "title",
                     },
                 },
                 {
@@ -73,24 +73,89 @@ def custom_solve() -> Solver:
                     "tool": "search_authors_by_name",
                     "arguments": {
                         "name": "Jonathan Bragg",
+                        "fields": "name,authorId,paperCount",
+                    },
+                },
+                {
+                    "tool": "get_author_papers",
+                    "arguments": {
+                        "author_id": "2274102473",
+                        "fields": "title,authors",
+                    },
+                },
+                {
+                    "tool": "search_papers_by_relevance",
+                    "arguments": {
+                        "keyword": "deep learning",
+                        "fields": "title,authors,abstract",
+                    },
+                },
+                {
+                    "tool": "search_paper_by_title",
+                    "arguments": {
+                        "title": "CORE-Bench: Fostering the Credibility of Published Research Through a Computational Reproducibility Agent Benchmark",
+                        "fields": "isOpenAccess,title,fieldsOfStudy,url,venue,year,tldr",
+                    },
+                },
+                {
+                    "tool": "snippet_search",
+                    "arguments": {
+                        "query": "Tasks require interpreting both text and image output from code. The vision-based questions require extracting results from attributes of figures, graphs, plots, or PDF tables.",
+                        "fields": "title",
+                    },
+                },
+                # mis-formatted requests; we expect they fail, but want to get a useful error message
+                {
+                    "tool": "get_paper_batch",
+                    "arguments": {
+                        "ids": ["arxiv:2409.11363", "arxiv:2409.11364"],
+                        "fields": "title,",
+                    },
+                },
+                {
+                    "tool": "search_authors_by_name",
+                    "arguments": {
+                        "name": "Jonathan Bragg",
                         "fields": "name,author_id,paper_count",
+                    },
+                },
+                {
+                    "tool": "get_paper",
+                    "arguments": {
+                        "paperId": "4c913d59d150fe7581386b87dfd9f90448a9adee",
+                    },
+                },
+                {
+                    "tool": "get_paper",
+                    "arguments": {
+                        "paper_id": 7857660,
                     },
                 },
             ]
 
+            failed = []
             for call in calls:
                 logger.info(
                     "Calling tool %s with arguments %s", call["tool"], call["arguments"]
                 )
                 start_time = time.perf_counter()
                 t = tbyname[call["tool"]]
-                result = await t(**call["arguments"])
+                try:
+                    result = await t(**call["arguments"])
+                except:
+                    logger.exception(
+                        f"Error calling tool {call['tool']} with arguments {call['arguments']}"
+                    )
+                    failed.append(call)
+                    continue
                 end_time = time.perf_counter()
                 logger.info(
                     "Call finished in %.2f seconds. Result: %s",
                     end_time - start_time,
                     result,
                 )
+
+            logger.info("Failed calls: %s", failed)
         except Exception as e:
             logger.exception(f"Error while fetching tools:")
             state.output.completion = f"Error while fetching tools: {e}"
