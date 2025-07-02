18fb687bbfe010d188d88d4edc090355af839a51
Mike D'Arcy <miked@allenai.org>
2025-07-02 13:55:28 -0700

    Add my custom solver for debugging

--- astabench/solvers/llm.py
+++ astabench/solvers/llm.py
@@ -1,4 +1,12 @@
+import logging
+import time
+
 from inspect_ai.solver import Solver, chain, generate, solver, system_message
+from inspect_ai.tool import ToolDef
+
+from astabench.tools import make_s2api_toolsource
+
+logger = logging.getLogger(__name__)
 
 
 @solver
@@ -15,3 +23,78 @@ def llm_with_prompt(system_prompt: str | None = None) -> Solver:
         chainlist.insert(0, system_message(system_prompt))
 
     return chain(chainlist)
+
+
+@solver
+def custom_solve() -> Solver:
+    async def solve(state, generate):
+
+        try:
+            ts = make_s2api_toolsource()
+
+            tl = await ts.tools()
+
+            tbyname = dict()
+
+            for t in tl:
+                td = ToolDef(t)
+                tbyname[td.name] = t
+                logger.info(f"Tool: {td.name} - {td.description} - {td.parameters}")
+
+            calls = [
+                {
+                    "tool": "get_paper",
+                    "arguments": {
+                        "paper_id": "arxiv:2409.11363",
+                        "fields": "title,authors,abstract",
+                    },
+                },
+                {
+                    "tool": "get_paper",
+                    "arguments": {
+                        "paper_id": "4c913d59d150fe7581386b87dfd9f90448a9adee",
+                    },
+                },
+                {
+                    "tool": "get_paper_batch",
+                    "arguments": {
+                        "ids": ["arxiv:2409.11363", "arxiv:2409.11364"],
+                        "fields": "title,",
+                    },
+                },
+                {
+                    "tool": "get_citations",
+                    "arguments": {
+                        "paper_id": "arxiv:2409.11363",
+                        "fields": "title",
+                    },
+                },
+                {
+                    "tool": "search_authors_by_name",
+                    "arguments": {
+                        "name": "Jonathan Bragg",
+                        "fields": "name,author_id,paper_count",
+                    },
+                },
+            ]
+
+            for call in calls:
+                logger.info(
+                    "Calling tool %s with arguments %s", call["tool"], call["arguments"]
+                )
+                start_time = time.perf_counter()
+                t = tbyname[call["tool"]]
+                result = await t(**call["arguments"])
+                end_time = time.perf_counter()
+                logger.info(
+                    "Call finished in %.2f seconds. Result: %s",
+                    end_time - start_time,
+                    result,
+                )
+        except Exception as e:
+            logger.exception(f"Error while fetching tools:")
+            state.output.completion = f"Error while fetching tools: {e}"
+
+        return state
+
+    return solve
