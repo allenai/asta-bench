8a7db3feb1bc97fe9b196be37d89343b1f3c86b8
Dany Haddad <danyhaddad43@gmail.com>
2025-07-25 20:18:35 +0000

    allow showing of all citations

--- citation_eval_ui/citation_eval_ui/main.py
+++ citation_eval_ui/citation_eval_ui/main.py
@@ -162,7 +162,10 @@ async def annotate(request: Request, filename: str, row: Optional[int] = None, c
                 sorted_citations.insert(supporting_count, citation)
             else:
                 other_citations.append(citation)
-
+        
+        # Add other citations at the end
+        sorted_citations.extend(other_citations)
+        
         # Replace the citations list with sorted version
         current_row.citations = sorted_citations
         
